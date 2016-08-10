de9a3eed6bb5d4a7934fb2d3ed585646f94f4380
jdonato <jdonato@etsy.com>
2016-08-09 21:16:27 -0400

    initial

---
+++ README.md
@@ -0,0 +1,16 @@
+## Montage
+
+A little shell script to:
+
+- Take a bunch of jpgs in a folder...
+- Make them all landscape
+- Center crop them
+- Lay them out into one giant jpg
+
+## Requirements
+
+imagemagick (and ghostscript because one of the imagemagick commands requires it)
+
+## Usage
+
+Drop it in a folder and ```./montage.sh```
---
+++ montage.sh
@@ -0,0 +1,19 @@
+echo "Making all photos landscape"
+for f in *.jpg; 
+do 
+    h=`identify -format "%h" $f` 
+    w=`identify -format "%w" $f` 
+    echo "$f height: $h width: $w"
+    if [ $h -gt $w ]
+    then
+        `convert -rotate 90 $f $f`
+    fi
+done
+for f in *.jpg; 
+do 
+    echo "cropping $f"
+    `convert $f -gravity center -crop 3120x3120+0+0 +repage $f-cropped.jpg`
+done
+
+echo "Creating montage"
+result=`montage *-cropped.jpg -geometry 3120x3120+3+3 montage.jpg`
